3f7c77dbc89625b7ed072bbcc880cb1f75f64eaa
Alper Üste <alperuste@gmail.com>
2019-12-04 16:39:56 +0200

    Updated Card Picker
    first test

--- Assets/Scripts/CardPicker.cs
+++ Assets/Scripts/CardPicker.cs
@@ -8,10 +8,19 @@ public class CardPicker : MonoBehaviour
     // first list, spawn it in the game screen and store it in second list.
     // When first list is empty, switch lists
 
+    public List<GameObject> CardList;
+    public List<GameObject> UsedCards;
+    public GameObject ActiveCard;
+    public GameObject PickedCard;
+    public Card cardScript;
+    //LAST CARD ekle, card degistirmeye onu kullan
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //GetCards();
+        ChooseCard();
     }
 
     // Update is called once per frame
@@ -19,4 +28,58 @@ public class CardPicker : MonoBehaviour
     {
         
     }
+    
+    public void ChooseCard()
+    {
+        //PickedCard = CardList[Random.Range(0, CardList.Count)]; // Choose random from list.
+        //print("first picked card " + PickedCard);
+        //if (UsedCards.Contains(PickedCard))
+        //{
+        //    while (UsedCards.Contains(PickedCard))
+        //    {
+        //        PickedCard = CardList[Random.Range(0, CardList.Count)];
+        //        print("next picked card " + PickedCard);
+        //    }
+        //    ActiveCard = PickedCard;
+        //    print("Card active " + ActiveCard);
+        //    UsedCards.Add(ActiveCard);
+        //    print("Card added to UsedCard List " + ActiveCard);
+        //    print(ActiveCard.GetComponent<Card>().dialogue); //Get text from cards script
+        //    print(ActiveCard.GetComponent<Card>().score);
+        //    //UsedCards.Add(PickedCard);
+        //    //CardList.Remove(PickedCard);
+
+        //}
+        //else if (!UsedCards.Contains(PickedCard))
+        //{
+        //    ActiveCard = PickedCard;
+        //    print("Card active " + ActiveCard);
+        //    UsedCards.Add(ActiveCard);
+        //    print("Card added to UsedCard List " + ActiveCard);
+        //}
+        if (CardList.Count >=1)
+        {
+            PickedCard = CardList[Random.Range(0, CardList.Count)]; // Choose random from list.
+            UsedCards.Add(PickedCard);
+            CardList.Remove(PickedCard);
+            ActiveCard = PickedCard;
+            print(ActiveCard.GetComponent<Card>().dialogue); //Get text from cards script
+            print(ActiveCard.GetComponent<Card>().score);
+        }
+        else if (UsedCards.Count >= 1)
+        {
+            PickedCard = UsedCards[Random.Range(0, UsedCards.Count)]; // Choose random from list.
+            CardList.Add(PickedCard);
+            UsedCards.Remove(PickedCard);
+            ActiveCard = PickedCard;
+            print(ActiveCard.GetComponent<Card>().dialogue); //Get text from cards script
+            print(ActiveCard.GetComponent<Card>().score);
+        }
+
+    }
+
+    //public void ActivateCard()
+    //{
+
+    //}
 }
